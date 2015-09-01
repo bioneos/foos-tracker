23f095cef7d27a11f28611c896e9da0a19d5d34d
Mike Smith <msmith@bioneos.com>
2015-09-01 14:56:23 -0500

    Add the leaderboard route

--- app/controllers/player.js
+++ app/controllers/player.js
@@ -49,6 +49,56 @@ router.get('/all', function (req, res, next) {
     res.json(ret);
   });
 });
+
+// Leaderboard of all players
+router.get('/leaderboard', function(req, res, next) {
+  var leaderboard = [] ;
+  var games = {} ;
+
+  // Load up our players for our game summary table
+  db.Player.findAll({ 'include' : [ db.Game, db.Goal ]}).then(function (players) {
+    players.forEach(function(player) {
+      // Get the players games
+      var playerObj = {
+        'name' : player.name,
+        'nick' : player.nick,
+        'retired' : player.retired,
+        'wins' : 0,
+        'loses' : 0,
+        'embs' : 0,
+        'goals' : 0
+      } ;
+
+      player.Goals.forEach(function(goal) {
+        if (!games[goal.GameId]) games[goal.GameId] = {} ;
+        if (!games[goal.GameId][goal.PlayerId]) games[goal.GameId][goal.PlayerId] = 0 ;
+        games[goal.GameId][goal.PlayerId]++ ;
+      }) ;
+
+      // For every game this player is in, tally our goals/wins/losses/embarrassments
+      player.Games.forEach(function(game) {
+        playerObj.goals += player.Goals.length ;
+
+        // Handle our outcomes
+        if (player.Goals.length === game.threshold)
+        {
+          playerObj.wins++ ;
+        }
+        else
+        {
+          playerObj.loses++ ;
+          if (player.Goals.length === 0) playerObj.embs++ ;
+        }
+      }) ;
+
+      leaderboard.push(playerObj) ;
+    }) ;
+
+    console.log(games) ;
+
+  });
+}) ;
+
 router.get('/:id', function (req, res, next) {
   db.Player.find({ where: { id: req.params.id }}).then(function (player) {
     // Get Player's last game?
